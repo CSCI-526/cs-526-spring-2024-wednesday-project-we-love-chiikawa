65d8c3dd229dce5bd4d8328d7718c58582c69931
alleny526 <alleny@usc.edu>
2024-03-23 14:25:12 -0700

    Added Guide scene and Home scene

---
+++ Light of Life/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadHomeScene()
+    {
+        SceneManager.LoadScene("Home");
+    }
+
+    public void LoadGuideScene()
+    {
+        SceneManager.LoadScene("Guide");
+    }
+
+    public void LoadLevelScene()
+    {
+        SceneManager.LoadScene("levelDesignTest");
+    }
+}
